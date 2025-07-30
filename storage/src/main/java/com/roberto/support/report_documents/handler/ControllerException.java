dd15be45337648288718b32097fbe056e96fc899
Roberto Vagner <53801038+robertovagner775@users.noreply.github.com>
2025-07-29 21:53:17 -0300

    Adicionando teste de sucesso da funcionalidade de upload de arquivo

--- storage/src/main/java/com/roberto/support/report_documents/handler/ControllerException.java
+++ storage/src/main/java/com/roberto/support/report_documents/handler/ControllerException.java
@@ -3,7 +3,10 @@ package com.roberto.support.report_documents.handler;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ControllerException extends  {
+public class ControllerException extends ResponseEntityExceptionHandler  {
+
+
 }
---
+++ storage/src/main/java/com/roberto/support/report_documents/handler/exceptions/ExceptionExtension.java
@@ -0,0 +1,11 @@
+package com.roberto.support.report_documents.handler.exceptions;
+
+import java.io.IOException;
+
+public class ExceptionExtension extends IOException {
+
+
+    ExceptionExtension(String message) {
+        super(message);
+    }
+}
