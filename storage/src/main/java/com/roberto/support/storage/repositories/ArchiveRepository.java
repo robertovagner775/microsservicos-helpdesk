12d2c31088e669b5408ca7d7849684113449697f
Roberto Vagner <53801038+robertovagner775@users.noreply.github.com>
2025-09-22 21:12:01 -0300

    commit

--- storage/src/main/java/com/roberto/support/storage/repositories/ArchiveRepository.java
+++ storage/src/main/java/com/roberto/support/storage/repositories/ArchiveRepository.java
@@ -1,10 +1,11 @@
 package com.roberto.support.storage.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.roberto.support.storage.models.Archive;
 
-
+@Repository
 public interface ArchiveRepository extends JpaRepository<Archive, String> {
     
 }
--- storage/src/main/java/com/roberto/support/storage/repositories/FileTicketRepository.java
+++ storage/src/main/java/com/roberto/support/storage/repositories/FileTicketRepository.java
@@ -5,8 +5,10 @@ import com.roberto.support.storage.models.FileTicket;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface FileTicketRepository extends JpaRepository<FileTicket, String> {
+@Repository
+public interface FileTicketRepository extends JpaRepository<FileTicket, Integer> {
 
     Optional<FileTicket> findByIdTicket(Integer idTicket);
 }
